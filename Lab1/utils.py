b85d633f75308d44a266e04abb775e18e48ca5b0
moyer <moyersdmcp@gmail.com>
2023-04-28 12:27:16 +0800

    Initial commit

---
+++ Lab1/utils.py
@@ -0,0 +1,12 @@
+
+def generate_dots(num: int, seed: int):
+    """
+    生成2D正方形平面点集合，默认边长为100
+    :param seed: 随机种子
+    :param num: 数目
+    :return: 点集合
+    """
+    pass
+
+def draw_performance_graph():
+    pass
